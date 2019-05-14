378aae4f4fb47c955187fbf2742e7296169f5670
zhang <1151215420@qq.com>
2019-05-14 14:12:18 +0800

    commit code

---
+++ src/main/java/com/polycis/main/mapper/admin/OssAdminMapper.java
@@ -0,0 +1,16 @@
+package com.polycis.main.mapper.admin;
+
+import com.polycis.main.entity.admin.OssAdmin;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author ${author}
+ * @since 2019-05-14
+ */
+public interface OssAdminMapper extends BaseMapper<OssAdmin> {
+
+}
---
+++ src/main/java/com/polycis/main/service/admin/impl/OssAdminServiceImpl.java
@@ -0,0 +1,20 @@
+package com.polycis.main.service.admin.impl;
+
+import com.polycis.main.entity.admin.OssAdmin;
+import com.polycis.main.mapper.admin.OssAdminMapper;
+import com.polycis.main.service.admin.IOssAdminService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author ${author}
+ * @since 2019-05-14
+ */
+@Service
+public class OssAdminServiceImpl extends ServiceImpl<OssAdminMapper, OssAdmin> implements IOssAdminService {
+
+}
