1a3d6f26ce72a2fb215ccd9b1f4e6b2fbdaffa1e
zhangwei <zhangwei>
2019-05-14 14:21:47 +0800

    更新01

--- src/main/java/com/polycis/main/controller/consumer/ConsumerController.java
+++ src/main/java/com/polycis/main/controller/consumer/ConsumerController.java
@@ -1,6 +1,17 @@
 package com.polycis.main.controller.consumer;
 
+import com.polycis.main.controller.user.UsersController;
+import io.swagger.annotations.Api;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consumer")
+@Api(value = "ConsumerController", description = "系统模块:客户信息")
 public class ConsumerController {
+    protected static Logger LOG = LoggerFactory.getLogger(ConsumerController.class);
 
 
 }
