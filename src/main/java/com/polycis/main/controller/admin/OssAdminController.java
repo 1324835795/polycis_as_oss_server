c9b16176258f54c9d6dbe6f8cc1f2d7ec69a89e4
zhang <1151215420@qq.com>
2019-05-14 14:43:04 +0800

    commit code

---
+++ src/main/java/com/polycis/main/controller/admin/OssAdminController.java
@@ -0,0 +1,209 @@
+package com.polycis.main.controller.admin;
+
+
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.enums.SqlLike;
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.netflix.discovery.converters.Auto;
+import com.polycis.main.common.ApiResult;
+import com.polycis.main.common.CommonCode;
+import com.polycis.main.common.MainConstants;
+import com.polycis.main.common.interceptor.RequestHolder;
+import com.polycis.main.common.page.PageInfoVO;
+import com.polycis.main.common.page.RequestVO;
+import com.polycis.main.entity.Users;
+import com.polycis.main.entity.admin.OssAdmin;
+import com.polycis.main.mapper.admin.OssAdminMapper;
+import com.polycis.main.service.admin.IOssAdminService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author qiaokai
+ * @since 2019-05-14
+ */
+@RestController
+@RequestMapping("/admin")
+public class OssAdminController {
+
+
+    @Autowired
+    private IOssAdminService iOssAdminService;
+
+    @ApiOperation(value = "oss用户登录", notes = "oss用户登录接口")
+    @PostMapping("/login")
+    public ApiResult login(@RequestBody Users uss, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        ApiResult apiResult = new ApiResult<>();
+        OssAdmin ossAdmin = new OssAdmin();
+        ossAdmin.setLoginname(uss.getLoginname());
+        ossAdmin.setPassword(uss.getPassword());
+        Users userss = iUsersService.isUser(uss);
+        if (userss != null) {
+
+            String key = UUID.randomUUID().toString().substring(0, 16);
+            LOG.info("往redis里塞值key:{},value:{}", key, userss.toString());
+            ApiResult result = redisFeignClient.set(key, JSON.toJSONString(userss), MainConstants.TOKEN_LIFETIME);
+
+            if (result.getCode() == CommonCode.SUCCESS.getKey()) {
+                Cookie cookie = new Cookie(MainConstants.COOKIE_NAME, key);
+                cookie.setMaxAge(MainConstants.COOKIE_LIFETIME);
+                cookie.setPath("/");
+                cookie.setDomain("");
+                // cookie.setHttpOnly(true);
+                httpServletResponse.addCookie(cookie);
+            } else {
+                apiResult.setMsg(CommonCode.ERROR.getValue());
+                apiResult.setCode(CommonCode.ERROR.getKey());
+                apiResult.setMsg("redis服务错误,请联系管理员");
+                return apiResult;
+            }
+            apiResult.setData(userss);
+            return apiResult;
+        }
+        apiResult.setMsg(CommonCode.ERROR.getValue());
+        apiResult.setCode(CommonCode.ERROR.getKey());
+        apiResult.setMsg("错误");
+        return apiResult;
+    }
+
+
+    @ApiOperation(value = "用户登出", notes = "登录登出")
+    @PostMapping("/logout")
+    public ApiResult logout(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws IOException {
+        ApiResult apiResult = new ApiResult<>();
+        Cookie cookie = new Cookie(MainConstants.COOKIE_NAME, null);
+        cookie.setPath("/");
+        cookie.setMaxAge(-1);
+        httpServletResponse.addCookie(cookie);
+        return apiResult;
+    }
+
+    @ApiOperation(value = "用户启用/不启用", notes = "用户启用/不启用")
+    @PostMapping("/active")
+    public ApiResult active(@RequestBody Users users) throws IOException {
+        Users currentUser = RequestHolder.getCurrentUser();
+        ApiResult apiResult = new ApiResult<>();
+        if (currentUser.getRole().contains(MainConstants.SYS) && currentUser.getOrg().equals(users.getOrg())) {
+            users.setIsStart(0);
+            iUsersService.updateById(users);
+            return apiResult;
+        }
+        apiResult.setMsg(CommonCode.AUTH_LIMIT.getValue());
+        apiResult.setCode(CommonCode.AUTH_LIMIT.getKey());
+        return apiResult;
+    }
+
+    @ApiOperation(value = "oss添加用户", notes = "oss添加用户")
+    @PostMapping("/add")
+    public ApiResult add(@RequestBody OssAdmin ossAdmin) throws IOException {
+        OssAdmin currentUser = RequestHolder.getCurrentUser();
+        ApiResult apiResult = new ApiResult<>();
+        // 添加的用户只是用户,不能管理员添加管理员
+        if (currentUser.getRole().contains(MainConstants.SYS)) {
+
+            if(ossAdmin.getType()==1)
+                ossAdmin.setRole(MainConstants.USER);
+            if(ossAdmin.getType()==2)
+                ossAdmin.setRole(MainConstants.SYS);
+            ossAdmin.setStart(1);
+            boolean b = false;
+            try {
+                b = iOssAdminService.insert(ossAdmin);
+                return apiResult;
+            } catch (Exception e) {
+                apiResult.setMsg("用户登录名重复");
+                apiResult.setCode(CommonCode.ERROR.getKey());
+                return apiResult;
+            }
+        }
+        apiResult.setMsg(CommonCode.AUTH_LIMIT.getValue());
+        apiResult.setCode(CommonCode.AUTH_LIMIT.getKey());
+        return apiResult;
+    }
+
+
+    @ApiOperation(value = "删除oss用户", notes = "删除oss用户")
+    @PostMapping("/delete")
+    public ApiResult delete(@RequestBody OssAdmin ossAdmin) throws IOException {
+        OssAdmin currentUser = RequestHolder.getCurrentUser();
+        ApiResult apiResult = new ApiResult<>();
+        if(currentUser.getId().equals(ossAdmin.getId())){
+            apiResult.setCode(CommonCode.ERROR.getKey());
+            apiResult.setMsg("不能删除自己");
+        }
+
+        if (currentUser.getRole().contains(MainConstants.SYS)) {
+            ossAdmin.setDelete(MainConstants.DELETETED);
+            iOssAdminService.updateById(ossAdmin);
+            return apiResult;
+        }else {
+            apiResult.setMsg(CommonCode.AUTH_LIMIT.getValue());
+            apiResult.setCode(CommonCode.AUTH_LIMIT.getKey());
+            return apiResult;
+        }
+
+    }
+
+    @ApiOperation(value = "修改oss用户", notes = "修改oss用户")
+    @PostMapping("/update")
+    public ApiResult update(@RequestBody OssAdmin ossAdmin) throws IOException {
+        OssAdmin currentUser = RequestHolder.getCurrentUser();
+        ApiResult apiResult = new ApiResult<>();
+        // 操作用户是管理员 且 被操作用户是type=1用户
+        if (currentUser.getRole().contains(MainConstants.SYS)) {
+
+            boolean b = iOssAdminService.updateById(ossAdmin);
+            if (b) {
+                return apiResult;
+            }
+            apiResult.setCode(CommonCode.ERROR.getKey());
+            apiResult.setMsg(CommonCode.ERROR.getValue());
+            return apiResult;
+        }else {
+            apiResult.setMsg(CommonCode.AUTH_LIMIT.getValue());
+            apiResult.setCode(CommonCode.AUTH_LIMIT.getKey());
+            return apiResult;
+        }
+
+    }
+
+
+    @ApiOperation(value = "oss用户列表", notes = "oss用户列表")
+    @PostMapping("/list")
+    public ApiResult list(@RequestBody RequestVO requestVO) throws IOException {
+        OssAdmin currentUser = RequestHolder.getCurrentUser();
+        EntityWrapper<OssAdmin> usersEntityWrapper = new EntityWrapper<>();
+        OssAdmin ossAdmin = JSON.parseObject(JSON.toJSONString(requestVO.getData()), OssAdmin.class);
+        /*if (null != ossAdmin.getLoginname() && !"".equals(ossAdmin.getLoginname())) {
+            usersEntityWrapper.like("loginname", ossAdmin.getLoginname(), SqlLike.RIGHT);
+        }*/
+        usersEntityWrapper.eq("is_delete", MainConstants.UN_DELETE);
+        usersEntityWrapper.orderBy("create_time desc");
+        ApiResult apiResult = new ApiResult<>();
+        PageInfoVO pageInfo = requestVO.getPageInfo();
+        Page<OssAdmin> usersPage = new Page<>(pageInfo.getCurrentPage(), pageInfo.getPageSize());
+        Page<OssAdmin> usersPage1 = iOssAdminService.selectPage(usersPage, usersEntityWrapper);
+        apiResult.setData(usersPage1);
+        return apiResult;
+    }
+
+
+}
+
