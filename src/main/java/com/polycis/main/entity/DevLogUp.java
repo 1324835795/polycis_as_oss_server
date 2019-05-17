35b6331869c1a2712c573aa355c38a7653e0e949
eziozhou <m13718591023@163.com>
2019-05-17 13:58:01 +0800

    设备日志模块

---
+++ src/main/java/com/polycis/main/entity/DevLogUp.java
@@ -0,0 +1,73 @@
+package com.polycis.main.entity;
+
+/**
+ * @author : Wenyu Zhou
+ * @version : v1.0
+ * @date : 2019/5/17
+ * description : 描述
+ */
+public class DevLogUp {
+    private String modifyTime;
+    private String decodeData;
+    private String encodeData;
+    private int platform;
+    private String devName;
+    private String devUuid;
+    private int status;
+
+    public String getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(String modifyTime) {
+        this.modifyTime = modifyTime.length()==21?modifyTime.substring(0,19):modifyTime;
+    }
+
+    public String getDecodeData() {
+        return decodeData;
+    }
+
+    public void setDecodeData(String decodeData) {
+        this.decodeData = decodeData;
+    }
+
+    public String getEncodeData() {
+        return encodeData;
+    }
+
+    public void setEncodeData(String encodeData) {
+        this.encodeData = encodeData;
+    }
+
+    public int getPlatform() {
+        return platform;
+    }
+
+    public void setPlatform(int platform) {
+        this.platform = platform;
+    }
+
+    public String getDevName() {
+        return devName;
+    }
+
+    public void setDevName(String devName) {
+        this.devName = devName;
+    }
+
+    public String getDevUuid() {
+        return devUuid;
+    }
+
+    public void setDevUuid(String devUuid) {
+        this.devUuid = devUuid;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
--- src/main/java/com/polycis/main/service/db3/impl/LogServiceImpl.java
+++ src/main/java/com/polycis/main/service/db3/impl/LogServiceImpl.java
@@ -3,6 +3,7 @@ package com.polycis.main.service.db3.impl;
 import com.polycis.main.common.ApiResult;
 import com.polycis.main.common.CommonCode;
 import com.polycis.main.common.page.RequestVO;
+import com.polycis.main.entity.DevLogUp;
 import com.polycis.main.mapper.db3.LogMapper;
 import com.polycis.main.service.db3.IlogService;
 import org.springframework.stereotype.Service;
@@ -36,9 +37,32 @@ public class LogServiceImpl implements IlogService {
     }
 
     @Override
-    public ApiResult<Map<String, Object>> sekectDevLogState(RequestVO requestVO) {
+    public ApiResult<Map<String, Object>> selectDevLogState(RequestVO requestVO) {
         List<Map<String,Object>> list = logMapper.selectDevLogState(requestVO);
         int total = logMapper.selectDevLogStateCount(requestVO);
+        Map<String,Object> map = new HashMap<>(16);
+        map.put("records",list);
+        map.put("total",total);
+        ApiResult<Map<String,Object>> apiResult = new ApiResult<>(CommonCode.SUCCESS);
+        apiResult.setData(map);
+        return apiResult;
+    }
+
+    @Override
+    public ApiResult<Map<String, Object>> selectDevLogUp(RequestVO requestVO) {
+        List<DevLogUp> list = logMapper.selectDevLogUp(requestVO);
+        int total = logMapper.selectDevLogUpCount(requestVO);
+        Map<String,Object> map = new HashMap<>(16);
+        map.put("records",list);
+        map.put("total",total);
+        ApiResult<Map<String,Object>> apiResult = new ApiResult<>(CommonCode.SUCCESS);
+        apiResult.setData(map);
+        return apiResult;
+    }
+    @Override
+    public ApiResult<Map<String, Object>> selectDevLogDown(RequestVO requestVO) {
+        List<DevLogUp> list = logMapper.selectDevLogDown(requestVO);
+        int total = logMapper.selectDevLogDownCount(requestVO);
         Map<String,Object> map = new HashMap<>(16);
         map.put("records",list);
         map.put("total",total);
