c51f1675c2d2dcccd6eb5c72f205803364ac5363
zhang <1151215420@qq.com>
2019-05-23 10:48:23 +0800

    Merge branch 'master' of 192.168.10.19:polycis/polycis_as_oss_server
    # Conflicts:
    #	src/main/java/com/polycis/main/entity/admin/OssAdmin.java

--- src/main/java/com/polycis/main/common/filter/FilterConfigs.java
+++ src/main/java/com/polycis/main/common/filter/FilterConfigs.java
@@ -1,15 +1,19 @@
 package com.polycis.main.common.filter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class FilterConfigs {
+
+    @Autowired
+    private XssFilter xssFilter;
     @Bean
     public FilterRegistrationBean filterRegistrationBean(){
         FilterRegistrationBean bean = new FilterRegistrationBean();
-        bean.setFilter(new XssFilter());
+        bean.setFilter(xssFilter);
         bean.addUrlPatterns("/*");
         return bean;
     }
